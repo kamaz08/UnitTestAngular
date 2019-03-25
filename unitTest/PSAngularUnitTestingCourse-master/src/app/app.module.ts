9a57d482990ab88141162bc7d48d9f974d433036
kamaz-home <kamaz08@gmail.com>
2019-03-25 23:46:48 +0100

    rxFlex

---
+++ unitTest/PSAngularUnitTestingCourse-master/src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatButtonModule, MatCardModule} from '@angular/material'
+
+
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
+import { HeroService }          from './hero.service';
+import { MessageService }       from './message.service';
+import { MessagesComponent }    from './messages/messages.component';
+import { StrengthPipe } from './strength/strength.pipe';
+import { HeroComponent } from './hero/hero.component';
+import { MyCardComponent } from './my-card/my-card.component';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
+
+    FlexLayoutModule,
+
+    BrowserAnimationsModule,
+    
+    MatCardModule, MatButtonModule
+
+
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    HeroSearchComponent,
+    StrengthPipe,
+    HeroComponent,
+    MyCardComponent
+  ],
+  providers: [ HeroService, MessageService ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ unitTest/PSAngularUnitTestingCourse-master/src/app/my-card/my-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-my-card',
+  templateUrl: './my-card.component.html',
+  styleUrls: ['./my-card.component.css']
+})
+export class MyCardComponent implements OnInit {
+
+  constructor() { }
+  @Input() elo: string;
+  ngOnInit() {
+  }
+
+}
